6964b153be3b51a801f72e8586aa993725fe6f5c
root <root@hdpr01wn05.localdomain>
2016-11-04 09:12:40 +0800

    Updated.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['HOST'] = 'lc.pic.net.tw';
-$config['DEMO_HOST'] = 'lab.pic.net.tw';
+$config['HOST'] = 'collector.yourdomain.com';
+$config['DEMO_HOST'] = 'demo.yourdomain.ccom';
 $config['DEBUG'] = true;
 $config['CC_IDS'] = array("CC-WEB1", "CC-WEB2");
 $config['KAFKA_BROKERS'] = 'kafka01:9020,kafka02:9200,kafka3:9200,kafka4:9200,kafka5:9200';
